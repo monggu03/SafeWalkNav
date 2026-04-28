a74cee581f82b8a66163a652ee9e9e5ba22280fe
monggu03 <leoisthestart@gmail.com>
2026-04-28 14:49:54 +0900

    기본값 재수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,3 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
 }
-
-// OneDrive가 build 폴더를 동기화하면서 Gradle 빌드 깨뜨리는 문제 방지
-// build 출력을 OneDrive 밖(C:\SafeWalkBuild)으로 리다이렉트
-allprojects {
-    layout.buildDirectory = file("C:/SafeWalkBuild/${project.name}")
-}
